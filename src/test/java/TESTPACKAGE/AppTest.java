eaaa30100ebeba7f07cbf1b1b67ba943f6fccfb5
Angelo <40685162@live.napier.ac.uk>
2024-11-13 15:51:43 +0000

    ReadMe update for integration

---
+++ src/test/java/TESTPACKAGE/AppTest.java
@@ -0,0 +1,36 @@
+package TESTPACKAGE;
+
+import TESTPACKAGE.App;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AppTest {
+    static App app;
+
+    @BeforeAll
+    static void init() {
+        app = new App();
+    }
+
+    @Test
+    void printCitiesTestNull() {
+        app.printCityReport(null);
+    }
+
+    @Test
+    void printCitiesTestNormal() {
+        ArrayList<City> cities = new ArrayList<>();
+        City city = new City(458, "Glasgow", "GBR", "Scotland", 619680);
+        cities.add(city);
+        city = new City(459, "Liverpool", "GBR", "England", 461000);
+        cities.add(city);
+        city = new City(460, "Edinburgh", "GBR", "Scotland", 450180);
+        cities.add(city);
+        app.printCityReport(cities);
+    }
+
+}
